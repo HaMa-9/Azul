80b8638297c6735032225fc35bb3c53b5edbdf55
HannahMa <hannahmagin@outlook.de>
2020-04-15 15:54:43 +0200

    bag basic functionality

---
+++ src/main/kotlin/com/kotlin/Azul/Elements/Bag.kt
@@ -0,0 +1,40 @@
+class Bag() {
+    private var stones = mutableListOf<Stone>()
+
+    enum class Stone {
+        BLUE, RED, YELLOW, BLACK, GREEN
+    }
+
+    init {
+        stones.addAll(listOf(
+                Stone.BLUE, Stone.RED, Stone.YELLOW, Stone.BLACK, Stone.GREEN,
+                Stone.BLUE, Stone.RED, Stone.YELLOW, Stone.BLACK, Stone.GREEN,
+                Stone.BLUE, Stone.RED, Stone.YELLOW, Stone.BLACK, Stone.GREEN,
+                Stone.BLUE, Stone.RED, Stone.YELLOW, Stone.BLACK, Stone.GREEN,
+                Stone.BLUE, Stone.RED, Stone.YELLOW, Stone.BLACK, Stone.GREEN,
+                Stone.BLUE, Stone.RED, Stone.YELLOW, Stone.BLACK, Stone.GREEN))
+    }
+
+    private fun containsAtLeastN(n: Int): Boolean = stones.size >= n
+
+    fun shuffle() = stones.shuffle()
+
+    fun pickN(n: Int): List<Stone>? {
+        if (n < 0) {
+            print("n may not be negative")
+        } else {
+            if (!containsAtLeastN(n)) {
+                print("add stones from table first")
+            } else {
+                val picked = stones.take(n)
+                stones = stones.drop(n) as MutableList<Stone>
+                return picked
+            }
+        }
+        return null
+    }
+
+    fun add(stonesToAdd: List<Stone>) {
+        stones.addAll(stonesToAdd)
+    }
+}
